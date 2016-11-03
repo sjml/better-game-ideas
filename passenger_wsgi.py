9c13505c35b2cd9da5356186fd144be41bc887f8
Shane Liesegang <shane@techie.net>
2016-11-03 14:49:08 -0700

    removing unneeded run command

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -6,5 +6,3 @@ if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 from better_game_ideas_web import app as application
 
-if __name__ == '__main__':
-    application.run(debug=False)
